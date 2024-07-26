65d3673ec09ee6db886a2600d2bda1feb97d4959
eakrkmu <kranthi.kumar@ericsson.com>
2024-07-26 13:01:11 +0200

    Adding more files

---
+++ pythonProject/main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def print_number(number):
+    print(f'My number,{number}')
+
+
+def print_index(index):
+    print("print the 0th index", index[0])
+    print("print the 1st index", index[1])
+    print("print the 2nd index", index[2])
+    print("print the 3rd index", index[3])
+    print("print the 4th index", index[4])
+    print("print the 5th index", index[5])
+    print("print the 6th index", index[6])
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print_number('1234567890')
+    print_index('Kranthi')
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
