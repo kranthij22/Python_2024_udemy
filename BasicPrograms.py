90407b2c51a2ff963184177763a44e2be8e08f96
Seema Negi <41466478+SeemaNegi@users.noreply.github.com>
2024-07-28 14:25:15 +0200

    Basic programing questions

---
+++ BasicPrograms.py
@@ -0,0 +1,9 @@
+# Write a program to find the simple interest
+
+
+# Write a program to find the factorial of a number . Example: factorial of 6 is 6*5*4*3*2*1 which is 720.
+
+
+# Write a program to iterate over each character in a string and print it. For example: string = hello , output = h,e,l,l,o
+
+
